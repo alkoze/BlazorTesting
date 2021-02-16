1088653c17fecd139696ad6b8416dea17e301e3a
unknown <aleksandr.koz99@gmail.com>
2021-02-16 14:32:12 +0200

    Custom validation in entity Publisher

--- Shared/Publisher.cs
+++ Shared/Publisher.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Test.Shared
 {
-    public class Publisher
+    public class Publisher : IValidatableObject
     {
         public Guid PublisherId { get; set; }
 
@@ -18,9 +18,23 @@ namespace Test.Shared
         public string PublisherName { get; set; } = default!;
 
         [Required(ErrorMessage = "Year of creation is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "YearFunded can not be less than 0.")]
         public int YearFunded { get; set; } = default!;
 
+        [Range(0, int.MaxValue, ErrorMessage = "YearClosed can not be less than 0.")]
         public int? YearClosed { get; set; } = null;
         public ICollection<Book> PublisherBooks { get; set; } = new List<Book>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (YearFunded > YearClosed)
+            {
+                yield return new ValidationResult("YearClosed must not be less than YearFunded");
+            }
+            if (YearClosed > DateTime.Now.Year || YearFunded > DateTime.Now.Year)
+            {
+                yield return new ValidationResult($"Yearclosed and YearFunded must be less than {DateTime.Now.Year}");
+            }
+        }
     }
 }
